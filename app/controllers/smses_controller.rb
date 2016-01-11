d1befdab796f2c8998781e5f53d4f0b43ec3c371
Aniket Garg <aniketgarg@outlook.com>
2016-01-12 00:46:53 +0530

    add silly greetings

--- app/controllers/smses_controller.rb
+++ app/controllers/smses_controller.rb
@@ -2,8 +2,9 @@ class SmsesController < ApplicationController
   require 'twilio-ruby'
   
   def sms_quickstart
+    greetings = ['How\'s it going?', 'What\'s up?', 'Good to hear from you!', 'Sup?', 'Howdy!', 'Well hello!', 'Look what the cat dragged in!']
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Hey Monkey. Thanks for the message!"
+      r.Message "#{greetings.sample} Thanks for the message!"
     end
     render xml: twiml.text
   end
